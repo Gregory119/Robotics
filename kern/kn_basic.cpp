a061752cdc7c38fb3e22db6f96645553d985b657
Gregory119 <gregorykylejones@yahoo.com>
2016-08-18 22:44:19 +0200

    basic kernel loop

---
+++ kern/kn_basic.cpp
@@ -0,0 +1,12 @@
+#include "kn_basic.h"
+
+using namespace KN;
+
+//----------------------------------------------------------------------//
+void KernBasic::run()
+{
+  while (d_comp->stayRunning()) 
+    {
+      //add debug assert if d_comp == nullptr
+    }
+}
