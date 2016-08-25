6dc15e4d456e345149cb40f23b9642addea5773c
Gregory119 <gregorykylejones@yahoo.com>
2016-08-25 23:26:04 +0200

    main.d

--- coms/main.d
+++
@@ -1,4 +0,0 @@
-coms_main.o coms_main.d : coms_main.cpp coms_serialjoystick.h \
- /home/greg/Robotics/joystick/js_joystick.h \
- /home/greg/Robotics/joystick/js_common.h \
- /home/greg/Robotics/kern/kn_basic.h
