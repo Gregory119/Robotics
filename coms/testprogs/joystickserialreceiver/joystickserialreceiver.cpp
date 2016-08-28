370ae491bf49306be76fcf361398c0f48b98b80c
Gregory119 <gregorykylejones@yahoo.com>
2016-08-28 22:47:15 +0200

    added cast to print out characters in a readable way

--- coms/testprogs/joystickserialreceiver/joystickserialreceiver.cpp
+++ coms/testprogs/joystickserialreceiver/joystickserialreceiver.cpp
@@ -12,12 +12,15 @@ int main()
   if (js_receiver.init("/dev/ttyAMA0",
 		       baud))
     {
-      if(js_receiver.readSerialEvent(js_event))
+      while (1)
 	{
-	  std::cout << "time [ms]: " << js_event.time_ms << std::endl;
-	  std::cout << "value: " << js_event.value << std::endl;
-	  std::cout << "type: " << (int)js_event.type << std::endl;
-	  std::cout << "number: " << (int)js_event.number << std::endl;
+	  if(js_receiver.readSerialEvent(js_event))
+	    {
+	      std::cout << "time [ms]: " << js_event.time_ms << std::endl;
+	      std::cout << "value: " << js_event.value << std::endl;
+	      std::cout << "type: " << (int)js_event.type << std::endl;
+	      std::cout << "number: " << (int)js_event.number << std::endl;
+	    }
 	}
     }
 }
