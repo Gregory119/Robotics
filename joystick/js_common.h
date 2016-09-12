15c1b76635c6bf2fff20fa5e56760d1826ee745e
Gregory119 <gregorykylejones@yahoo.com>
2016-09-12 22:30:13 +0200

    removed unnecesary event structures

--- joystick/js_common.h
+++ joystick/js_common.h
@@ -47,13 +47,7 @@ namespace JS
   //	__u8 number;	/* axis/button number */
   //};
 
-  struct JSEvent
-  {
-    uint32_t time = 0;
-    int16_t value = 0;
-    uint8_t type = 0;
-    uint8_t number = 0;
-  };
+  typedef js_event JSEvent;
 
   struct JSEventMinimal
   {
@@ -61,34 +55,6 @@ namespace JS
     int8_t value = 0;
     uint8_t type = 0;
     uint8_t number = 0;
-  };
-
-  //will not need this in the future
-  struct AxisEvent
-  {
-    AxisEvent(const js_event &event);
-    
-    AxisID getID() { return d_id; }
-    double getValue() { return d_value; }
-
-    private:
-    double d_value = 0.0; //from -1.0 to 1.0 
-    AxisID d_id = AXIS_UNKOWN; 
-    double d_min_value;
-    double d_max_value;
-  };
-
-  //will not need this in the future
-  struct ButtonEvent
-  {
-    ButtonEvent(const js_event &event);
-    
-    ButtonID getID() { return d_id; }
-    bool getValue() { return d_value; }
-
-    private:
-    bool d_value = false; 
-    ButtonID d_id = BUTTON_UNKNOWN; 
   };
 };
 #endif
