1c42c3c8360975f225891320517c2615ddbe07bf
Gregory119 <gregorykylejones@yahoo.com>
2016-09-12 22:29:16 +0200

    added wait time error

--- coms/testprogs/joystickserialreceiver/joystickserialreceiver.cpp
+++ coms/testprogs/joystickserialreceiver/joystickserialreceiver.cpp
@@ -6,7 +6,8 @@ int main()
 {
   int baud = 9600;
 
-  COMS::JoystickReceiver js_receiver;
+  unsigned wait_time_error_ms = 500;
+  COMS::JoystickReceiver js_receiver(wait_time_error_ms);
   JS::JSEventMinimal js_event;
   
   if (js_receiver.init("/dev/ttyAMA0",
