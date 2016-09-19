23b8c32983d6a560d1ebb3a563879027c15b69b9
Gregory119 <gregorykylejones@yahoo.com>
2016-09-19 15:22:22 +0200

    robot and pin defines

---
+++ projects/bushbot/robot/pins.h
@@ -0,0 +1,15 @@
+#ifndef PINS_H
+#define PINS_H
+
+namespace PIN
+{
+  // the wiring pi pin numbering is used according to
+  // http://wiringpi.com/pins/
+  unsigned steering = 1;
+  unsigned motor = 7;
+  //unsigned roll = ??;
+  //unsigned pitch = ??;
+  //unsigned yaw = ??;
+};
+
+#endif
