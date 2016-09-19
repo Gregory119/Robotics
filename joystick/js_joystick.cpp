5fc2cbf06dbe426146ccb0643ee5510135677bd5
Gregory119 <gregorykylejones@yahoo.com>
2016-09-19 12:35:50 +0200

    Using the threaded function in an attached state by using a pointer to the thread to call join. Renamed some of the functions and split them into separate functions. Used asserts for some condition checks.

--- joystick/js_joystick.cpp
+++ joystick/js_joystick.cpp
@@ -4,11 +4,14 @@
 
 #include <fcntl.h> //open
 #include <unistd.h> //close
+#include <assert.h>
 #include <thread>
 
 #include <iostream> //std::cout
 
 using namespace JS;
+
+static std::thread *s_t = nullptr;
 
 //----------------------------------------------------------------------//
 JoyStick::JoyStick(JoyStickOwner* o, const char *dev_name)
@@ -20,47 +23,83 @@ JoyStick::JoyStick(JoyStickOwner* o, const char *dev_name)
 JoyStick::~JoyStick()
 {
   std::cout << "~JoyStick()" << std::endl;
-  if (d_running) { stop(); }
-  if (d_open) { close(d_js_desc); }
+  stop();
+}
+
+//----------------------------------------------------------------------//
+void JoyStick::closeDevice()
+{
+  assert(d_open);
+
+  close(d_js_desc);
+  d_open = false;
+}
+
+//----------------------------------------------------------------------//
+bool JoyStick::openDevice()
+{
+  assert(!d_open);
+
+  d_js_desc = open(d_dev_name,O_RDONLY); //including O_NONBLOCK in the flags will cause io operations to return without waiting (returns error if not available straight away). No blocking causes io operations to wait for a return.
+  /*
+    This function is a cancellation point in multi-threaded programs. This is a problem if the thread allocates some resources (like memory, file descriptors, semaphores or whatever) at the time open is called. If the thread gets canceled these resources stay allocated until the program ends. To avoid this calls to open should be protected using cancellation handlers. 
+  */
+  
+  if (d_js_desc == s_error)
+    {
+      d_open=false; 
+      return false;
+    }
+  else 
+    d_open=true;
+  
+  return true;
 }
 
 //----------------------------------------------------------------------//
 bool JoyStick::init()
 {
-  if (!d_open)
-    {
-      d_js_desc = open(d_dev_name,O_RDONLY); //including O_NONBLOCK in the flags will cause io operations to return without waiting (returns error if not available straight away). No blocking causes io operations to wait for a return.
-      /*
-	This function is a cancellation point in multi-threaded programs. This is a problem if the thread allocates some resources (like memory, file descriptors, semaphores or whatever) at the time open is called. If the thread gets canceled these resources stay allocated until the program ends. To avoid this calls to open should be protected using cancellation handlers. 
-      */
-    }
-
-  if (d_js_desc == s_error) { d_open=false; }
-  else { d_open=true; }
-  
-  return d_open;
+  return openDevice();
 }
 
 //----------------------------------------------------------------------//
 void JoyStick::run()
 {
+  assert(!d_running);
+    
   d_running = true;
-  std::thread t(&JoyStick::threadFunc,
+  std::thread t(threadFunc,
 		d_thread_shutdown.get_future(),
 		this);
-  t.detach();
+  s_t = &t;
 }
 
 //----------------------------------------------------------------------//
 void JoyStick::stop()
 {
+  if (d_running)
+  stopThread();
+
+  if (d_open)
+  closeDevice();
+}
+
+//----------------------------------------------------------------------//
+void JoyStick::stopThread()
+{
+  assert(d_running);
+
   d_running = false;
   d_thread_shutdown.set_value(true);
+  
+  assert(s_t!=nullptr);
+  if (s_t->joinable()) 
+    s_t->join();
 }
 
 //----------------------------------------------------------------------//
 void JoyStick::threadFunc(std::future<bool> shutdown,
-			  const JoyStick* js)
+			  const JoyStick *const js)
 {
   while (shutdown.wait_for(std::chrono::nanoseconds(0)) != 
 	 std::future_status::ready)
