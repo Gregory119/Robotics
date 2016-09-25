2c20e7cba7dbdc4791300a7b0d297fea4bef05ed
Gregory119 <gregorykylejones@yahoo.com>
2016-09-25 20:31:37 +0200

    Added definitions of joystick limits for minimal event values.

---
+++ joystick/js_common.cpp
@@ -0,0 +1,14 @@
+#include "js_common.h"
+
+namespace JS
+{
+  namespace EventMinimal
+  {
+    const char lever_max_out = -127;
+    const char lever_max_in = 127;
+    const char axis_max_left = -127;
+    const char axis_max_up = -127;
+    const char axis_max_right = 127;
+    const char axis_max_down = 127;
+  };
+};
