2beb56a8af7c890d368ca6a620f0a6bc32654f78
Gregory119 <gregorykylejones@yahoo.com>
2016-09-29 18:15:33 +0200

    Corrected syntax of if statements. Re-worded some logs.

---
+++ projects/bushbot/scripts/start_bushbot.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+#this script currently does not handle the case when there is usb controller read error
+#possible future solutions:
+# 1) restart binaries if there are no events after a certain period (eg. 60s)
+
+dir=/Bushbot
+check_time_sec=5 
+#uncomment one of the following variables to run as a robot or basestation
+binary="basestation"
+#binary="robot"
+
+while true; do
+    if [ "$binary" == "basestation" ]; then
+	if [ -z $(pidof $binary) ]; then
+	    pkill xboxdrv
+	    echo "Starting xbox controller driver."
+	    xboxdrv -d -s --quiet -l 2 > /dev/null 2>&1 #do not care about output
+	    cd $dir
+	    echo "Starting $binary."
+	    ./$binary > /dev/null 2>&1
+	fi
+    elif [ "$binary" == "robot" ]; then
+	if [ -z $(pidof $binary) ]; then
+	    echo "Starting $binary."
+	    cd $dir
+	    ./$binary > /dev/null 2>&1
+	fi
+    fi
+    sleep $check_time_sec
+done
+
