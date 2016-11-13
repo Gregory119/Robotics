8335a2dbb00b6918c2679f954f9d85b60d777d89
Gregory119 <gregorykylejones@yahoo.com>
2016-11-13 15:08:49 +0200

    Added conditional check for xboxdrv if the basestation is not running.

--- projects/bushbot/scripts/start_bushbot.sh
+++ projects/bushbot/scripts/start_bushbot.sh
@@ -4,7 +4,7 @@
 #possible future solutions:
 # 1) restart binaries if there are no events after a certain period (eg. 60s)
 
-dir=/Bushbot
+project_dir=/Bushbot
 check_time_sec=5 
 #uncomment one of the following variables to run as a robot or basestation
 binary="basestation"
@@ -13,17 +13,18 @@ binary="basestation"
 while true; do
     if [ "$binary" == "basestation" ]; then
 	if [ -z $(pidof $binary) ]; then
-	    pkill xboxdrv
-	    echo "Starting xbox controller driver."
-	    xboxdrv -d -s --quiet -l 2 > /dev/null 2>&1 & #do not care about output
-	    cd $dir
+	    if [ -z $(pidof xboxdrv) ]; then
+		echo "Starting xbox controller driver."
+		xboxdrv -d -s --quiet -l 2 > /dev/null 2>&1 & #do not care about output
+	    fi
+	    cd $project_dir
 	    echo "Starting $binary."
 	    ./$binary > /dev/null 2>&1
 	fi
     elif [ "$binary" == "robot" ]; then
 	if [ -z $(pidof $binary) ]; then
 	    echo "Starting $binary."
-	    cd $dir
+	    cd $project_dir
 	    ./$binary > /dev/null 2>&1
 	fi
     fi
