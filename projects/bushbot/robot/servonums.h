40e24a94d2379e28cd3fd717b68c99b2f89de926
Gregory119 <gregorykylejones@yahoo.com>
2017-01-12 23:59:58 +0200

    Changed the controller to use rt and lt to control the motor speed

--- projects/bushbot/robot/servonums.h
+++ projects/bushbot/robot/servonums.h
@@ -5,8 +5,8 @@ namespace SERVONUM
 {
   // numbers are according to the servoblaster naming convention
   // https://github.com/richardghirst/PiBits/tree/master/ServoBlaster
-  unsigned steering = 2;
-  unsigned motor = 0;
+  unsigned steering = 0;
+  unsigned motor = 5;
   //unsigned roll = ??;
   //unsigned pitch = ??;
   //unsigned yaw = ??;
