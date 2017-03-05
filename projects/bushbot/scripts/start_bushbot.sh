21d5958ba43ce9810147d962dbc30048a40312c5
Gregory119 <gregorykylejones@yahoo.com>
2017-03-05 13:24:18 +0200

    Deleted some files

--- projects/bushbot/scripts/start_bushbot.sh
+++ projects/bushbot/scripts/start_bushbot.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
-#this script currently does not handle the case when there is usb controller read error
-#possible future solutions:
-# 1) restart binaries if there are no events after a certain period (eg. 60s)
-
 project_dir=/Bushbot
+startscript_dir=/usr/startscipts
 check_time_sec=5 
 #uncomment one of the following variables to run as a robot or basestation
 binary="basestation"
@@ -13,16 +10,15 @@ binary="basestation"
 while true; do
     if [ "$binary" == "basestation" ]; then
 	if [ -z $(pidof $binary) ]; then
-	    #if [ -z $(pidof xboxdrv) ]; then
-		#echo "Starting xbox controller driver."
-		#xboxdrv -d -s --quiet -l 2 > /dev/null 2>&1 & #do not care about output
-	    #fi
 	    cd $project_dir
 	    echo "Starting $binary."
 	    ./$binary > /dev/null 2>&1
 	fi
     elif [ "$binary" == "robot" ]; then
 	if [ -z $(pidof $binary) ]; then
+	    if [ -z $(pidof servod) ]; then
+		./$startscript_dir/start_servoblaster.sh > /dev/null 2>&1
+	    fi
 	    echo "Starting $binary."
 	    cd $project_dir
 	    ./$binary > /dev/null 2>&1
