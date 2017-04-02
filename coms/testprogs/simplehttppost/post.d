058fe6af0b8448bcd19b6516ee27cc75647ffaf1
root <gregorykylejones@yahoo.com>
2017-04-02 17:21:14 +0200

    files not needed for commit

--- coms/testprogs/simplehttppost/post.d
+++
@@ -1,8 +0,0 @@
-post.o post.d : post.cpp ../../../coms/coms_simplehttppost.h \
- /home/rasppi_xcompile/extra_libs_install/curl/curl.h \
- /home/rasppi_xcompile/extra_libs_install/curl/curlver.h \
- /home/rasppi_xcompile/extra_libs_install/curl/curlbuild.h \
- /home/rasppi_xcompile/extra_libs_install/curl/curlrules.h \
- /home/rasppi_xcompile/extra_libs_install/curl/easy.h \
- /home/rasppi_xcompile/extra_libs_install/curl/multi.h \
- /home/rasppi_xcompile/extra_libs_install/curl/curl.h
