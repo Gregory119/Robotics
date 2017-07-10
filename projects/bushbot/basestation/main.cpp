1eda2c570c7d9c90d580d3507b3acb3b9e954ea8
root <gregorykylejones@yahoo.com>
2017-07-10 22:50:10 +0200

    Changed name

---
+++ projects/bushbot/basestation/main.cpp
@@ -0,0 +1,25 @@
+#include "djs_serialjoystick.h"
+#include "kn_stdkernel.h"
+
+#include <iostream>
+
+int main()
+{
+  KERN::StdKernel k;
+  
+  int baud = 9600;
+  std::string serial = "/dev/ttyAMA0";
+  std::string joystick = "/dev/input/js0";
+
+  D_JS::JoystickTransmitter js_tran;
+  js_tran.setResendEventTimeoutMs(200);
+  if (!js_tran.init(serial,
+		    baud,
+		    joystick))
+    {
+      std::cout<<"failed to initialize"<<std::endl;
+      return 0;
+    }
+
+  return k.run();
+}
