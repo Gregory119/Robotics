81a384862d09d7af9971c5e4cba6c3f7f70582c6
root <gregorykylejones@yahoo.com>
2017-10-17 18:30:26 +0200

    Deleted unnecessary files.

--- drivers/gopro/testprogs/modecontroller/main.d
+++
@@ -1,3 +0,0 @@
-main.o main.d : main.cpp ../../../../drivers/gopro/dgp_modecontroller.h \
- ../../../../drivers/gopro/dgp_gopro.h \
- ../../../../drivers/gopro/dgp_types.h ../../../../kern/kn_stdkernel.h
