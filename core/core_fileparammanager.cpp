0bc34c3120d4d02ed78360145c7762b5d0973443
root <gregorykylejones@yahoo.com>
2018-03-02 15:29:33 -0500

    The file is now opened and closed after every public command request, so that any set parameters will be written to the file before returning from the command. Completed the function that sets a parameter. Added a function to remove quotation marks around the parameter value. Improved error handling and messages.

---
+++ core/core_fileparammanager.cpp
@@ -0,0 +1,298 @@
+#include "core_fileparammanager.h"
+
+#include <cassert>
+#include <cstring>
+#include <fstream>
+#include <iostream>
+#include <sstream>
+
+using namespace CORE;
+
+//----------------------------------------------------------------------//
+FileParamManager::FileParamManager(std::string file_path)
+  : d_file_path(std::move(file_path))
+{}
+
+//----------------------------------------------------------------------//
+std::string FileParamManager::getParam(const std::string& name_match)
+{
+  std::string empty_str;
+  if (hasError())
+    {
+      // LOG!!!!!
+      return empty_str;
+    }
+
+  std::fstream file(d_file_path);
+  if (file.fail())
+    {
+      std::ostringstream stream("FileParamManager - Failed to open the file '",
+				std::ios_base::app);
+      stream << d_file_path << "'.";
+      setError(Error::FileOpen, stream.str());
+      return empty_str;
+    }
+  
+  std::string value;
+  try
+    {
+      goToParamValue(file,name_match);
+      if (hasError())
+	{
+	  return empty_str;
+	}
+      
+      file >> value;
+
+      if (file.fail())
+	{
+	  std::ostringstream stream("getParam - Failed to extract the parameter value of the parameter '",
+				    std::ios_base::app);
+	  stream << name_match << "' from the file '" << d_file_path << "'.";
+	  setError(Error::ParamExtraction, stream.str());
+	  return empty_str;
+	}
+    }
+  catch (...)
+    {
+      std::ostringstream stream("getParam - Failed to extract the parameter value of the parameter '",
+				std::ios_base::app);
+      stream << name_match << "' from the file '" << d_file_path << "'.";
+      setError(Error::GetParamException, stream.str());
+      return empty_str;
+    }
+  return value;
+}
+
+//----------------------------------------------------------------------//
+void FileParamManager::setParamFromTo(const std::string& name_match,
+				      const std::string& new_value)
+{
+  if (hasError())
+    {
+      // LOG!!!!!
+      return;
+    }
+
+  std::fstream file(d_file_path);
+  if (file.fail())
+    {
+      std::ostringstream stream("FileParamManager - Failed to open the file '",
+				std::ios_base::app);
+      stream << d_file_path << "'.";
+      setError(Error::FileOpen, stream.str());
+      return;
+    }
+  
+  try
+    {
+      // search and get the parameter value position and value
+      std::iostream::pos_type val_pos = getParamValuePos(file, name_match);
+      if (file.fail())
+	{
+	  std::ostringstream stream("setParam - Failed to set the parameter value because of the following error: \n",
+				    std::ios_base::app);
+	  stream << getErrorMsg();
+	  setError(Error::SetFilePosIndicator, stream.str());
+	  return;
+	}
+      
+      file.seekg(val_pos); //set input pos indicator
+      if (file.fail())
+	{
+	  std::ostringstream stream("setParam - Failed to set the input position indicator to ",
+				    std::ios_base::app);
+	  stream << val_pos << "' in the file '" << d_file_path << "'.";
+	  setError(Error::SetFilePosIndicator, stream.str());
+	  return;
+	}
+  
+      std::string current_value;
+      file >> current_value;
+      if (file.fail())
+	{
+	  std::ostringstream stream("setParam - Failed to extract the parameter value of the parameter '",
+				    std::ios_base::app);
+	  stream << name_match << "' from the file '" << d_file_path << "'.";
+	  setError(Error::ParamExtraction, stream.str());
+	  return;
+	}
+
+      if (current_value == new_value)
+	{
+	  // nothing is needed to be done if the values are the same
+	  return;
+	}
+
+      // if the new content has a smaller length than the current content, then replace and pad to make the length the same, if necessary.
+      int length_diff = current_value.length()-new_value.length();
+      if (length_diff >= 0)
+	{
+	  std::cout << "length_diff >= 0" << std::endl;
+	  file.seekp(val_pos); // set output pos indicator
+	  if (file.fail())
+	    {
+	      std::ostringstream stream("setParam - Unexpected failure when setting the output position indicator to ",
+					std::ios_base::app);
+	      stream << val_pos << "' in the file '" << d_file_path << "'.";
+	      setError(Error::SetFilePosIndicator, stream.str());
+	      return;
+	    }
+
+	  file << new_value;
+	  for (int i=0; i<length_diff; ++i)
+	    {
+	      file << " ";
+	    }
+	  if (file.fail())
+	    {
+	      std::ostringstream stream("setParam - Unexpected failure when outputting the new parameter value ' ",
+					std::ios_base::app);
+	      stream << new_value << "' with " << length_diff << " padded spaces into the file '" << d_file_path << "'.";
+	      setError(Error::OutputToFile, stream.str());
+	      return;
+	    }
+	  return;
+	}
+  
+      // If the new content is longer in length than the current content, then copy and paste data:
+      // Copy all of the file starting from the end of the parameter value
+      file.seekg(val_pos+static_cast<std::streamoff>(current_value.length())); // set input pos indicator
+      if (file.fail())
+	{
+	  std::ostringstream stream("setParam - Unexpected failure when setting the input position indicator to ",
+				    std::ios_base::app);
+	  stream << val_pos+static_cast<std::streamoff>(current_value.length()) << " in the file" << d_file_path << "'.";
+	  setError(Error::SetFilePosIndicator, stream.str());
+	  return;
+	}
+      std::stringstream trailing_data;
+      trailing_data << file.rdbuf();
+      // Set the new parameter value and trailing data
+      // First reset the internal basic_stringbuf position of the new file data after the new parameter
+      trailing_data.seekp(0);
+      trailing_data.seekg(0);
+      file.seekp(val_pos); // set output pos indicator
+      file << new_value;
+      file << trailing_data.rdbuf();
+      if (file.fail())
+	{
+	  std::ostringstream stream("setParam - Unexpected failure when outputting the new file data to the file '");
+	  stream << d_file_path << "'.";
+	  setError(Error::OutputToFile, stream.str());
+	  return;
+	}
+    }
+  catch (...)
+    {
+      std::ostringstream stream("setParam - Unexpected exception while trying to set the new parameter value in the file '");
+      stream << d_file_path << "'.";
+      setError(Error::SetParamException, stream.str());
+      return;
+    }
+  return;
+}
+
+//----------------------------------------------------------------------//
+bool FileParamManager::removeQuotesFromTo(const std::string& orig,
+					  std::string& no_quotes)
+{
+  // strcmp returns 0 if the strings are the same
+  static constexpr char quote_one[] = "\'";
+  static constexpr char quote_two[] = "\"";
+  if ((orig.front() != quote_one[0] &&
+       orig.front() != quote_two[0]) ||
+      (orig.back() != quote_one[0] &&
+       orig.back() != quote_two[0]))
+    {
+      return false;
+    }
+
+  try
+    {
+      no_quotes = orig.substr(1,orig.length()-2);
+    }
+  catch (...)
+    {
+      assert(false);
+      return false;
+    }
+  return true;
+}
+
+//----------------------------------------------------------------------//
+std::iostream::pos_type FileParamManager::getParamValuePos(std::fstream& file,
+							   const std::string& name_match)
+{
+  file.seekg(0); // reset input stream position indicator
+
+  std::string line;
+  size_t line_match_pos = 0;
+  std::iostream::pos_type previous_ipos = 0;
+
+  try
+    {
+      while (1)
+	{
+	  previous_ipos = file.tellg();
+	  std::getline(file, line);
+
+	  if (file.fail())
+	    {
+	      std::ostringstream stream("getParamValuePos - Failed to extract characters from the file '");
+	      stream << d_file_path << "'.";
+	      setError(Error::FileCharacterExtraction, stream.str());
+	      return std::iostream::pos_type(-1);
+	    }
+      
+	  line_match_pos = line.find(name_match);
+	  if (line_match_pos != std::string::npos)
+	    {
+	      break; // found!
+	    }
+
+	  if (file.eof())
+	    {
+	      std::ostringstream stream("getParamValuePos - The parameter '",std::ios_base::app);
+	      stream << name_match << "' does not exist in the file '" << d_file_path << "'.";
+	      setError(Error::ParamNotFound,stream.str());
+	      return std::iostream::pos_type(-1);
+	    }
+	}
+    }
+  catch (...)
+    {
+      std::ostringstream stream("getParamValuePosException - Unexpected exception while trying to get the position index of the start of the value of the parameter '",std::ios_base::app);
+      stream << name_match << "' in the file '" << d_file_path << "'.";
+      setError(Error::GetParamValuePosException, stream.str());
+      return false;
+    }
+  size_t line_value_pos = line_match_pos+name_match.length();
+  return previous_ipos+static_cast<std::streamoff>(line_value_pos);
+}
+
+//----------------------------------------------------------------------//
+void FileParamManager::goToParamValue(std::fstream& file,
+				      const std::string& name_match)
+{
+  std::iostream::pos_type val_pos = getParamValuePos(file, name_match);
+  if (hasError())
+    {
+      std::ostringstream stream("goToParamValue - Failed to go to the parameter value of the parameter '",std::ios_base::app);
+      stream << name_match << "' in the file '"
+	     << d_file_path << "' because of the following error:\n"
+	     << getErrorMsg();
+      setError(Error::ParamNotFound,stream.str());
+      return;
+    }
+  file.seekg(val_pos); // set input indicator
+  file.seekp(val_pos); // set output indicator
+}
+
+//----------------------------------------------------------------------//
+void FileParamManager::setError(Error e, const std::string& msg)
+{
+  d_error = e;
+  d_error_msg = "FileParamManager::";
+  d_error_msg += msg;
+}
---
+++ core/core_fileparammanager.h
@@ -0,0 +1,71 @@
+#ifndef CORE_FILEPARAMMANAGER_H
+#define CORE_FILEPARAMMANAGER_H
+
+#include <fstream>
+#include <string>
+
+namespace CORE
+{
+  // A general assumption is that only one parameter and its associated value reside on each line.
+  class FileParamManager
+  {
+  public:
+    enum class Error
+    {
+      None,
+	FileOpen,
+	ParamNotFound,
+	FileCharacterExtraction,
+	ParamExtraction,
+	SetFilePosIndicator,
+	OutputToFile,
+	SetParamException,
+	GetParamException,
+	GetParamValuePosException
+    };
+    
+  public:
+    FileParamManager(std::string file_path);
+
+    // eg. pin_number=5, which implies name_match is "pin_number=" and the returned string is "5".
+    // The file is first opened, then the first occurence of name_match is searched for, and finally the file is closed when returning the found parameter string. It is assumed that there is one parameter per line; the returned value will contain all the line text after the name_match.
+    // On failure the returned string is empty. 
+    std::string getParam(const std::string& name_match);
+
+    void setParamFromTo(const std::string& name_match,
+			const std::string& new_value);
+
+    const std::string& getFilePath() const { return d_file_path; }
+    
+    // Removes the start and end quotes of a parameter string. Only double style quotes can be used.
+    // Eg. "five" changes to five. On error the no_quotes parameter is not modified.
+    // On success the no_quotes parameter is cleared and set.
+    static bool removeQuotesFromTo(const std::string& orig, std::string& no_quotes);
+
+    // All error related functions reflect all the errors that occured within the non-static functions
+    bool hasError() const { return d_error != Error::None; }
+    Error getError() const { return d_error; }
+
+    // Error messages include the file path, but do not include a new line character at the end.
+    std::string& getErrorMsg() { return d_error_msg; }
+
+  private:
+    // Returns pos_type(-1) on failure.
+    std::iostream::pos_type getParamValuePos(std::fstream& file,
+					     const std::string& name_match);
+
+    // Sets the input position indicator.
+    void goToParamValue(std::fstream& file,
+			const std::string& name_match);
+    
+    void setError(Error, const std::string& msg);
+    
+  private:
+    const std::string d_file_path;
+
+    Error d_error = Error::None;
+    std::string d_error_msg;
+  };
+};
+
+#endif
