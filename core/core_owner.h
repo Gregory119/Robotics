40cc76b6d85e4983751acc583016252b4fff7acc
root <gregorykylejones@yahoo.com>
2018-03-02 15:23:47 -0500

    Improved comments

--- core/core_owner.h
+++ core/core_owner.h
@@ -1,7 +1,7 @@
 #ifndef CORE_OWNER_H
 #define CORE_OWNER_H
 
-// If a move special member is declared, then the other is implicitly not-declared, and the copy constructor and operator are deleted. If the move constructor was declared here, then the default constructor would implicitly not declared.
+// If a move special member is declared, then the other is implicitly not-declared, and the copy constructor and operator are deleted. If the move constructor was declared here, then the default constructor would be implicitly not declared.
 // The private destructor is used to ensure inheritance and disable non-polymorphic use.
 #define OWNER_SPECIAL_MEMBERS(friend_name)				\
   protected:								\
