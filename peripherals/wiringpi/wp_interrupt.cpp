91c038f3d1310a8d1f1222c312361455fb5704b7
root <gregorykylejones@yahoo.com>
2018-03-19 19:33:35 -0400

    Fixed the wiringpi interrupt such that the callback functions work for each instance of the interrupt class. Currently only 6 simultaneous instances are supported.

--- peripherals/wiringpi/wp_interrupt.cpp
+++ peripherals/wiringpi/wp_interrupt.cpp
@@ -1,27 +1,73 @@
 #include "wp_interrupt.h"
 
+#include <cassert>
+#include <iostream>
+#include <vector>
 #include <wiringPi.h>
+
+static std::vector<std::function<void(void)>> s_callbacks;
 
 using namespace P_WP;
 
-//----------------------------------------------------------------------//
-Interrupt::Interrupt(PinNum pin,
-		     Mode mode,
-		     std::function<void()> callback)
-  : d_pin(pin),
-    d_mode(mode)
+template <unsigned index>
+void InterruptFunc(void)
 {
-  d_pin_state = digitalRead(static_cast<int>(d_pin));
-
-  if (callback.target<void(*)(void)>() == nullptr)
+  if (s_callbacks.empty())
     {
-      d_has_error = true;
+      assert(false);
       return;
     }
   
+  s_callbacks[index]();
+}
+
+//----------------------------------------------------------------------//
+Interrupt::Interrupt(Owner* o,
+		     PinNum pin,
+		     Mode mode)
+  : d_owner(o),
+    d_pin(pin),
+    d_mode(mode)
+{
+  d_pin_state = digitalRead(static_cast<int>(d_pin));
+  
+  s_callbacks.push_back([this](){
+      d_owner.call(&Interrupt::Owner::handleInterrupt,this);
+    });
+  
+  void(*f)(void) = nullptr;
+  switch (s_callbacks.size())
+    {
+    case 1:
+      f = InterruptFunc<0>;
+      break;
+
+    case 2:
+      f = InterruptFunc<1>;
+      break;
+      
+    case 3:
+      f = InterruptFunc<2>;
+      break;
+
+    case 4:
+      f = InterruptFunc<3>;
+      break;
+
+    case 5:
+      f = InterruptFunc<4>;
+      break;
+
+    case 0:
+    default:
+      assert(false);
+      // LOG: current interrupt count limit
+      return;
+    }
+
   if (wiringPiISR(static_cast<int>(pin),
-		  static_cast<int>(mode),
-		  *callback.target<void(*)(void)>()) < 0)
+		  getWiringPiMode(mode),
+		  f) < 0)
     {
       d_has_error = true;
     }
@@ -38,4 +84,23 @@ bool Interrupt::readPin()
 bool Interrupt::getCachedPinState()
 {
   return d_pin_state;
+}
+
+//----------------------------------------------------------------------//
+int Interrupt::getWiringPiMode(Mode mode)
+{
+  switch (mode)
+    {
+    case Mode::Rising:
+      return INT_EDGE_RISING;
+	
+    case Mode::Falling:
+      return INT_EDGE_FALLING;
+      
+    case Mode::Both:
+      return INT_EDGE_BOTH;
+    }
+  assert(false);
+  // LOG
+  d_has_error = true;
 }
--- peripherals/wiringpi/wp_interrupt.h
+++ peripherals/wiringpi/wp_interrupt.h
@@ -3,6 +3,8 @@
 
 #include "wp_pins.h"
 
+#include "core_owner.h"
+
 #include <functional>
 
 namespace P_WP
@@ -19,9 +21,20 @@ namespace P_WP
     };
 
   public:
-    Interrupt(PinNum, Mode, std::function<void()> callback);
+    class Owner
+    {
+      OWNER_SPECIAL_MEMBERS(Interrupt);
+      virtual void handleInterrupt(Interrupt*) = 0;
+    };
+    
+  public:
+    Interrupt(Owner*,
+	      PinNum,
+	      Mode);
     Interrupt(Interrupt&&) = delete;
 
+    SET_OWNER();
+    
     // should be checked after construction
     bool hasError() { return d_has_error; }
 
@@ -30,8 +43,25 @@ namespace P_WP
     
     PinNum getPinNum() { return d_pin; }
     Mode getMode() { return d_mode; }
+
+  private:
+    int getWiringPiMode(Mode);
+
+  private:
+    enum class FuncCount
+    {
+      One,
+	Two,
+	Three,
+	Four,
+	Five,
+	Six,
+	End
+	};
     
   private:
+    CORE::Owner<Interrupt::Owner> d_owner;
+    
     PinNum d_pin = PinNum::Unknown;
     Mode d_mode = Mode::Both;
     bool d_has_error = false;
