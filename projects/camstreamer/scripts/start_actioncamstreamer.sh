ea4e501b4503b2394c20b118b43bf6eacd332871
Gregory119 <gregorykylejones@yahoo.com>
2018-05-01 14:33:28 -0400

    Updated the binary name to be hidden. Add file system expansion on first startup. Added the default user configuration file.

--- projects/camstreamer/scripts/start_actioncamstreamer.sh
+++ projects/camstreamer/scripts/start_actioncamstreamer.sh
@@ -1,18 +1,28 @@
 #!/bin/bash
 
-project_dir=/actioncamstreamer
+set -e # exit shell on a simple command failure
+
+project_dir=/.actioncamstreamer
 binary="actioncamstreamer"
+expand_file=fs_expanded
 
-while true; do
-    if [ -z $(pidof $binary) ]; then
-	cd $project_dir
-	echo "Starting $binary."
-	./$binary > /dev/null 2>&1 &
-    fi
-    if [ -z $(pidof "omxplayer") ]; then
-	echo "Starting omxplayer"
-	omxplayer --live udp://:8554 > /dev/null 2>&1
-	# Run in the foreground because omxplayer has a timeout when no data has been received
-    fi
-done
-
+# First expand the file system, if it has not been done already, and then reboot.
+cd $project_dir
+if [ ! -e "$expand_file" ]
+then
+    touch $expand_file
+    raspi-config --expand-rootfs
+    shutdown -r now
+else
+    while true; do
+	if [ -z $(pidof $binary) ]; then
+	    #echo "Starting $binary."
+	    ./$binary > /dev/null 2>&1 &
+	fi
+	if [ -z $(pidof "omxplayer") ]; then
+	    #echo "Starting omxplayer"
+	    omxplayer --live udp://:8554 > /dev/null 2>&1
+	    # Run in the foreground because omxplayer has a timeout when no data has been received
+	fi
+    done
+fi
